b33ce7c0f7c5475bfea434145874d01ee3ca59f1
giriparveen <giriparveen@gmail.com>
2020-12-24 22:42:56 +0530

    First Commit

---
+++ LoggingDemo/Controllers/ErrorController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingDemo.Controllers
+{
+    public class ErrorController : Controller
+    {
+        private readonly ILogger _logger;
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            this._logger = logger;
+        }
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            var execptionDetails = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+            switch (statusCode)
+            {
+                case 404:
+                    _logger.LogWarning($"404 Error Occured. Path ={execptionDetails.OriginalPath}" +
+                        $"and the query string ={execptionDetails.OriginalQueryString}");
+                    break;
+                default:
+                    break;
+            }
+            return View();
+        }
+        [Route("Error")]
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+          
+            var execptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            _logger.LogError($"The Execption path {execptionDetails.Path} threw an exception {execptionDetails.Error}");
+            ViewBag.ExceptionPath = execptionDetails.Path;
+            ViewBag.ExceptionMessage = execptionDetails.Error;
+            ViewBag.StackTrace = execptionDetails.Error.StackTrace;
+            return View("Errors");
+        }
+    }
+}
---
+++ LoggingDemo/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LoggingDemo.Models;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this._logger = logger;
+          
+        }
+        public IActionResult Index()
+        {
+            _logger.LogTrace("Log Trace");
+            _logger.LogDebug("Log Debug");
+            _logger.LogInformation("Log Information");
+            _logger.LogWarning("Log Warning");
+            _logger.LogError("Log Error");
+            _logger.LogCritical("Log Critical");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
